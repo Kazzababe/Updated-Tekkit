56d68b261ac0fd0c07b509e35b755468049ecb31
BuildTools <unconfigured@null.spigotmc.org>
2017-01-21 11:27:55 -0600

    Initial commit

--- src/main/java/ravioli/gravioli/tekkit/machines/transport/TransportReceiver.java
+++
@@ -1,8 +0,0 @@
-package ravioli.gravioli.tekkit.machines.transport;
-
-import org.bukkit.block.BlockFace;
-
-public interface TransportReceiver {
-    boolean canReceiveItem(MovingItem item, BlockFace input);
-    void addMovingItem(MovingItem item, BlockFace input);
-}
